22f8dffaf690d9e9c525dd0266bd81ee49354160
supigithub <145343105+supigithub@users.noreply.github.com>
2024-08-01 23:34:16 +0530

    Create jsmain.js

---
+++ jsmain.js
@@ -0,0 +1,63 @@
+
+(function() {
+    "use strict";
+  
+    const onscroll = (el, listener) => {
+      el.addEventListener('scroll', listener)
+    }
+  
+   
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+      let position = window.scrollY + 200
+      navbarlinks.forEach(navbarlink => {
+        if (!navbarlink.hash) return
+        let section = select(navbarlink.hash)
+        if (!section) return
+        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+          navbarlink.classList.add('active')
+        } else {
+          navbarlink.classList.remove('active')
+        }
+      })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+  
+   
+    const scrollto = (el) => {
+      let header = select('#header')
+      let offset = header.offsetHeight
+  
+      let elementPos = select(el).offsetTop
+      window.scrollTo({
+        top: elementPos - offset,
+        behavior: 'smooth'
+      })
+    }
+  
+    
+    let selectHeader = select('#header')
+    let selectTopbar = select('#topbar')
+    if (selectHeader) {
+      const headerScrolled = () => {
+        if (window.scrollY > 100) {
+          selectHeader.classList.add('header-scrolled')
+          if (selectTopbar) {
+            selectTopbar.classList.add('topbar-scrolled')
+          }
+        } else {
+          selectHeader.classList.remove('header-scrolled')
+          if (selectTopbar) {
+            selectTopbar.classList.remove('topbar-scrolled')
+          }
+        }
+      }
+      window.addEventListener('load', headerScrolled)
+      onscroll(document, headerScrolled)
+    }
+  
+   
+   
+  
+  })()
